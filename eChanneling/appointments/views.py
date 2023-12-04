317e8d3f5ea11fef178ee9e6a56802d5ee8fe5ac
Pawan Sasidu <143354295+PawanSasindu@users.noreply.github.com>
2023-12-04 20:12:28 +0530

    Succefully upload service html file into server with creating paths and view urls

--- eChanneling/appointments/views.py
+++ eChanneling/appointments/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Home.html')
+def other_services(request):
+    return render(request, 'Other Service.html')
